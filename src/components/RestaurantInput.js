b1c772c75cc0878d397ee9f0f4d727fc3f12da32
David Strauch <dmaster18@gmail.com>
2021-01-17 00:32:43 +0000

    Automatically backed up by Learn

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { addRestaurant } from '../actions/restaurants';
 import { connect } from 'react-redux';
 
-
+const mapDispatchToProps
 
 export class RestaurantInput extends Component {
 
